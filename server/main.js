b377ea04942b21c34bb8643c87fe6df5024f4397
lukas nydegger <nydegger.lukas@gmail.com>
2016-11-14 15:43:09 +0100

    update version and add Email

--- server/main.js
+++ server/main.js
@@ -2,5 +2,5 @@ import { Meteor } from 'meteor/meteor';
 import '../imports/api/steps/index.js';
 
 Meteor.startup(() => {
-
+    process.env.MAIL_URL = "smtp://personal.finder.app%40gmail.com:$Welcome16@smtp.gmail.com";
 });
