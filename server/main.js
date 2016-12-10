f73bca507b169625115eff7abc8db8818e8d9d20
lukas nydegger <nydegger.lukas@gmail.com>
2016-12-10 16:25:10 +0100

    par

--- server/main.js
+++ server/main.js
@@ -7,6 +7,7 @@ import '../imports/api/questions-characteristcs/index';
 // import { Characteristics } from '../imports/api/characteristics/index.js';
 
 Meteor.startup(() => {
+    process.env.MAIL_URL = "smtp://personal.finder.app%40gmail.com:$Pfinder.@smtp.gmail.com";
     Accounts.config({sendVerificationEmail: true});
     var user = Accounts.findUserByUsername(process.env.DUSERNAME);
     if(!user){
