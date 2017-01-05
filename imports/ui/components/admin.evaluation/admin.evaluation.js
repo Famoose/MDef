0b4dff8ba6ef0787418713e704072ecff38d08a8
linderp <pascal.linder97@hotmail.com>
2017-01-05 14:59:13 +0100

    evaluation

--- imports/ui/components/admin.evaluation/admin.evaluation.js
+++ imports/ui/components/admin.evaluation/admin.evaluation.js
@@ -4,14 +4,15 @@ import templateUrl from './admin.evaluation.html';
 import {Categories} from '../../../api/categories/index.js';
 
 class AdminEvaluation {
-    constructor($scope,$state) {
+    constructor($scope,$state,CategoryUser) {
         $scope.viewModel(this);
 
-        Meteor.subscribe('categories');
-        Meteor.subscribe('users');
         this.$state = $state;
 
         var self=this;
+
+        var categoriesHandler=self.Meteor.subscribe('categories',generate());
+        var usersHandler=self.Meteor.subscribe('users',generate());
 
         this.helpers({
             categories() {
@@ -26,6 +27,45 @@ class AdminEvaluation {
             }
         });
 
+
+        function generate()
+        {
+            if(categoriesHandler.ready() && usersHandler.ready())
+            {
+                var data = {
+                    labels: ["January", "February", "March", "April", "May", "June", "July"],
+                    datasets: [
+                        {
+                            label: "My First dataset",
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255,99,132,1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1,
+                            data: [65, 59, 80, 81, 56, 55, 40],
+                        }
+                    ]
+                };
+                var ctx=
+                var myBarChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: data,
+                    options: []
+                });
+            }
+        }
     }
 }
 const name = 'adminEvaluation';
@@ -35,7 +75,7 @@ export default angular.module(name, [
 ])
     .component(name, {
         templateUrl,
-        controller: ['$scope','$state', AdminEvaluation]
+        controller: ['$scope','$state','CategoryUser', AdminEvaluation]
     })
     .config(['$stateProvider', config]);
 
