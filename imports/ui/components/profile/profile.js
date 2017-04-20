6f36969b84502e88d9d4c4d238ca03fecc358016
Lukas Nydegger <lukasnydegger@Crap.local>
2017-04-20 10:48:47 +0200

    chart

--- imports/ui/components/profile/profile.js
+++ imports/ui/components/profile/profile.js
@@ -8,7 +8,7 @@ import {Cluster} from '../../../api/cluster/index.js';
 import {Bubble} from '../../../api/bubble/index.js';
 import {Question} from '../../../api/question/index.js';
 
-import {Highcharts} from 'highcharts';
+import Highcharts from 'highcharts';
 import 'highcharts/modules/exporting';
 
 import templateUrl from './profile.html';
@@ -25,7 +25,7 @@ class Profile {
             if (err) {
                 alert(err);
             } else {
-                console.log(res);
+                this.res = res;
             }
         });
         this.helpers({
@@ -64,6 +64,93 @@ class Profile {
                 console.log(res);
             }
         })
+    }
+
+    generateChart(){
+        var chart = Highcharts.chart('chart-container', {
+
+            chart: {
+                type: 'column'
+            },
+
+            title: {
+                text: 'Highcharts responsive chart'
+            },
+
+            subtitle: {
+                text: 'Resize the frame or click buttons to change appearance'
+            },
+
+            legend: {
+                align: 'right',
+                verticalAlign: 'middle',
+                layout: 'vertical'
+            },
+
+            xAxis: {
+                categories: ['Apples', 'Oranges', 'Bananas'],
+                labels: {
+                    x: -10
+                }
+            },
+
+            yAxis: {
+                allowDecimals: false,
+                title: {
+                    text: 'Amount'
+                }
+            },
+
+            series: [{
+                name: 'Christmas Eve',
+                data: [1, 4, 3]
+            }, {
+                name: 'Christmas Day before dinner',
+                data: [6, 4, 2]
+            }, {
+                name: 'Christmas Day after dinner',
+                data: [8, 4, 3]
+            }],
+
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 500
+                    },
+                    chartOptions: {
+                        legend: {
+                            align: 'center',
+                            verticalAlign: 'bottom',
+                            layout: 'horizontal'
+                        },
+                        yAxis: {
+                            labels: {
+                                align: 'left',
+                                x: 0,
+                                y: -5
+                            },
+                            title: {
+                                text: null
+                            }
+                        },
+                        subtitle: {
+                            text: null
+                        },
+                        credits: {
+                            enabled: false
+                        }
+                    }
+                }]
+            }
+        });
+
+        $('#small').click(function () {
+            chart.setSize(400, 300);
+        });
+
+        $('#large').click(function () {
+            chart.setSize(600, 300);
+        });
     }
 }
 const name = 'profile';
